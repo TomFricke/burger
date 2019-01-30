b4ef96af2de8670dc7a4fcce0c0360984ac47dbf
Tom Fricke <tomfricke13@gmail.com>
2019-01-29 22:36:22 -0600

    dope

---
+++ db/seeds.sql
@@ -0,0 +1,4 @@
+insert into burgers_tbl(burger_name) values ('Cheese Burger');
+insert into burgers_tbl(burger_name) values ('Garlic Burger');
+insert into burgers_tbl(burger_name) values ('Bacon Cheese Burger');
+insert into burgers_tbl(burger_name) values ('Hamburger');
